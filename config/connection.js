0f8aac6522274eceba11407165b881987fa15b7d
Ross Gongaware <ross@gongaware.org>
2019-02-14 16:24:34 -0500

    Added heroku code

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,25 @@
 var sql = require('mysql');
+var connection;
 
-var connection = sql.createConnection({
-    host: "localhost",
-    port: 3306,
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = sql.createConnection({
+		host: "localhost",
+		port: 3306,
 
-    user: "root",
-    password: "root",
-    database: "sandwiches_db"
-});
+		user: "root",
+		password: "root",
+		database: "sandwiches_db"
+	});
+}
 
 connection.connect(err => {
-    if (err) {
-        throw err;
-    } else {
-        console.log("SQL CONNECTION SUCCESSFUL");
-    }
+	if (err) {
+		throw err;
+	} else {
+		console.log("SQL CONNECTION SUCCESSFUL");
+	}
 })
 
 module.exports = connection;
